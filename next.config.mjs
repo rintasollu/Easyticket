1d492281facb620f43c29f45f3345f867a992043
rintasollu <rintasollu2005@gmail.com>
2025-09-29 12:03:29 +0800

    overflow-x auto terbaru

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // 1. Tambahkan base path (nama repositori)
-    basePath: '/Easyticket',
-    
-    // 2. Tentukan jalur aset statis
-    assetPrefix: '/Easyticket/',
-    
-    // 3. Opsional: Hapus trailing slash jika tidak diperlukan
-    trailingSlash: true,
-    
-    output: 'export', // Penting: Mengaktifkan output statis (static export)
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
